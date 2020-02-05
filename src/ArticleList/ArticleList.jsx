cd501dd83869b9ae6fe39c5af3c94ec13a524840
XINYINGGG <emilytian95@gmail.com>
2020-02-05 12:43:09 -0800

    week-4

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import Button from './ArticleListItem'
 
 const ArticleList = props => {
   return (
     <ul>
       {props.articles.map(article => (
-        <li key={article.slug}>ArticleListItem component goes here</li>
+          <li key={article.slug}>
+              <h3>{article.title}</h3>
+              <p>{article.shortText}</p>
+              <time dateTime>
+                  {article.pubDate}
+                  <Button info={article} />
+              </time>
+          </li>
       ))}
     </ul>
   );
